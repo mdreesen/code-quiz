c17f4e8014cdc2bd3b698cb8fc969fa21777ce17
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-17 09:37:01 -0600

    changed the var question

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ let questions = [{
         imgSrc: "img/js.png",
         choiceA: "Variable means anything that can vary",
         choiceB: "What is a variable?",
-        choiceC: "Isn't this a science question?",
+        choiceC: "Is this a science question?",
         correct: "A"
     },
     {
