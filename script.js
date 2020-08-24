123ec83f56bed5bf0c2e41b8a066d38dc5ea7acc
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-24 17:27:07 -0600

    another push

--- script.js
+++ script.js
@@ -1,183 +1,131 @@
-// GETTING ALL ELEMENTS IN THE HTML
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+$(document).ready(function() {
+    // Handler for .ready() called.
 
-// CREATE QUESTIONS
-let questions = [{
-        question: "What does HTML stand for?",
-        imgSrc: "img/html.png",
-        choiceA: "Hypertext Markup Language",
-        choiceB: "Hypertex Mark Language",
-        choiceC: "I dont know",
-        correct: "A"
-    }, {
-        question: "What does CSS stand for?",
-        imgSrc: "img/css.png",
-        choiceA: "Cascading Style Sheet",
-        choiceB: "Cascading Style Sheets",
-        choiceC: "I don't know",
-        correct: "B"
-    }, {
-        question: "What does JS stand for?",
-        imgSrc: "img/js.png",
-        choiceA: "Just Saying",
-        choiceB: "What was the question",
-        choiceC: "JavaScript",
-        correct: "C"
-    },
-    {
-        question: "What is a variable in Javascript?",
-        imgSrc: "img/js.png",
-        choiceA: "Variable means anything that can vary",
-        choiceB: "What is a variable?",
-        choiceC: "Isn't this a science question?",
-        correct: "A"
-    },
-    {
-        question: "Would you rather have all farts be silent but EXTREMELY deadly, or all farts be harmless but EXTREMELY loud?",
-        imgSrc: "img/screenshot.png",
-        choiceA: "all farts be silent but EXTREMELY deadly",
-        choiceB: "all farts be harmless but EXTREMELY loud",
-        choiceC: "I'm done",
-        correct: "C"
-    }
-];
+    var questions = [{
+            question: "who is the best boss in the world?",
+            a: "Lesli Knope",
+            b: "Michael Scott",
+            c: "Big Bird",
+            correct: "Michael Scott"
 
-// create some variables
-// gets the questions, minus 1
-const lastQuestion = questions.length - 1;
-// keeps track on the question we are on
-let onQ = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-// 
-const gaugeUnit = gaugeWidth / questionTime;
-// this is our timer
-let TIMER;
-// Score will be set to 0
-let score = 0;
+        }, {
+            question: "Who made the best chili?",
+            a: "Kelly",
+            b: "Toby",
+            c: "Kevin",
+            correct: "Kevin"
+        },
+        {
+            question: "Who started the fire?",
+            a: "Dwight",
+            b: "Ryan",
+            c: "Michael",
+            correct: "Ryan"
+        }
+    ]
 
-// render a question
-function renderQuestion() {
-    // making the variable q so that we don't have to type in this line again
-    let q = questions[onQ];
+    var pointer = 0;
+    var score = 0;
+    var timer = 60;
+    var timerId;
 
-    // Insert each question "p" element to the HTML
-    question.innerHTML = "<p>" + q.question + "</p>";
-    // changes the image in the HTML
-    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-    // gets the choices
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
+    $("#quiz").hide();
 
-// starts quiz when start quiz is clicked
-start.addEventListener("click", startQuiz);
 
-// start quiz function, starts the quiz after the start quiz button is clicked
-function startQuiz() {
-    // blocks the start button
-    start.style.display = "none";
-    // renders the function
-    renderQuestion();
-    quiz.style.display = "block";
-    // renders the progress function
-    renderProgress();
-    // renders the renderCounter function
-    renderCounter();
-    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
-}
+    // -=- MADE THIS FUNCTION MYSELF...NOT SURE IF THIS IS RIGHT -=-
+    // make a start button, this is what is going to start a quiz
+    $('#start').on("click", function() {
+        $("#game").hide();
+        $('#quiz').show();
+        countDown();
+        things();
+    });
+    // use local storage for different high scores, get and set - only do this when the high score is up, at the end of the quiz
 
-// render progress
-function renderProgress() {
-    // loops through and renders the progress
-    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
-    }
-}
 
-// RENDER COUNTER FUNCTION
-function renderCounter() {
-    // questions time is 10 seconds
-    if (count <= questionTime) {
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    } else {
-        count = 0;
-        // This changes the progress color to red
-        answerIsWrong();
-        if (onQ < lastQuestion) {
-            // increment the question when done
-            onQ++;
-            renderQuestion();
-        } else {
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
+    function countDown() {
+        timer--;
+        $('#time').text(timer);
+
+        if (timer <= 0) {
+            clearInterval(timerId);
+            // work with timer to make sure quiz stops at 0
+            if (timer === 0) {
+
+            }
+
+            // check for high score
         }
     }
-}
 
-// checkAnwer
 
-function checkAnswer(answer) {
-    if (answer == questions[onQ].correct) {
-        // answer is correct
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    } else {
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
+    function things() {
+        if (questions[pointer].question === undefined) {
+            return
+        } else {
+            $("#question").text(questions[pointer].question)
+            $("#1").text(questions[pointer].a)
+            $("#2").text(questions[pointer].b)
+            $("#3").text(questions[pointer].c)
+        };
+
     }
-    count = 0;
-    if (onQ < lastQuestion) {
-        onQ++;
-        renderQuestion();
-    } else {
-        // end the quiz and show the score
-        clearInterval(TIMER);
-        scoreRender();
+
+    // bet practice is to save the username through the localStorage, put it to an array
+
+
+    // CREAT FUNCTION FOR THE TIMEOUT
+
+
+    $(".answer").on("click", function() {
+        // we wnt logic to check if the answer is right or wrong
+        if ($(this).text() === questions[pointer].correct) {
+            score++;
+            console.log(score);
+
+        } else {
+            // need to get rid of time, -10 seconds
+            timer -= 10;
+        }
+        pointer++;
+
+        if (pointer === questions.length) {
+            // just display score
+            localStorage.setItem('recentScore', score);
+            location.replace('highScore.html');
+        } else {
+            things();
+        }
+        // pointer++, check if we are equal to the array if statements, then call the function
+    })
+
+    function save() {
+        var saveInput = $('#userName').val();
+        console.log(saveInput);
+        var saveFinalScore = localStorage.getItem('recentScore');
+        // retreivinng the list of scores from localStorage
+        var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        console.log(saveFinalScore);
+
+        // -=- how to reconfigure, grab by the name of the user and many users that have done the quiz -=-
+        //splice(saveFinalScore, saveInput);
+
+        for (var i = 0; i < highScores.length; i++) {
+            var p = $('<p>');
+            p.text(saveInput);
+            p.append(saveFinalScore);
+            $('#scoreInfo').append(p);
+        }
+        localStorage.setItem(saveInput, saveFinalScore);
     }
-}
 
-// answer is correct
-function answerIsCorrect() {
-    document.getElementById(onQ).style.backgroundColor = "#0f0";
-}
+    $('#saveButton').on("click", function(e) {
+        save();
+    });
 
-// answer is Wrong
-function answerIsWrong() {
-    document.getElementById(onQ).style.backgroundColor = "#f00";
-}
+    things();
 
-// score render
-function scoreRender() {
-    scoreDiv.style.display = "block";
+    timerId = setInterval(countDown, 1000);
+    timer === 0, clearInterval();
 
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score / questions.length);
-
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-        (scorePerCent >= 60) ? "img/4.png" :
-        (scorePerCent >= 40) ? "img/3.png" :
-        (scorePerCent >= 20) ? "img/2.png" :
-        "img/1.png";
-
-    scoreDiv.innerHTML = "<img src=" + img + ">";
-    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
-}
+});
